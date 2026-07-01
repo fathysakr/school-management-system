10b86c759a22a401384928c9c8b8ffb8f53f3586
fathysakr <fathysakr@hotmail.com>
2026-07-01 14:03:52 +0300

    simplify: remove all traps from pdf-init.ts to test if temp task interferes

--- src/lib/pdf-init.ts
+++ src/lib/pdf-init.ts
@@ -9,41 +9,6 @@ export async function getPdfjs(): Promise<any> {
     const workerMod = await import('./pdf.worker.mjs');
     (globalThis as any).pdfjsWorker = workerMod;
     const pdfjsMod = await import('pdfjs-dist/build/pdf.mjs');
-
-    (globalThis as any).__pdfjsDiag = { traps: [] };
-
-    const origWorkerDestroy = pdfjsMod.PDFWorker.prototype.destroy;
-    pdfjsMod.PDFWorker.prototype.destroy = function () {
-      (globalThis as any).__pdfjsDiag.traps.push({
-        event: 'worker.destroy',
-        name: this.name,
-        time: Date.now(),
-        hadMH: this._messageHandler !== null,
-        destroyed: this.destroyed,
-        stack: new Error().stack?.split('\n').slice(1, 6).join('; '),
-      });
-      return origWorkerDestroy.apply(this, arguments as any);
-    };
-
-    try {
-      const tmpTask = pdfjsMod.getDocument({ data: new Uint8Array(1) });
-      const TaskClass = tmpTask.constructor;
-      tmpTask.destroy().catch(() => {});
-      const origTaskDestroy = TaskClass.prototype.destroy;
-      TaskClass.prototype.destroy = function () {
-        (globalThis as any).__pdfjsDiag.traps.push({
-          event: 'task.destroy',
-          time: Date.now(),
-          hasWorker: !!this._worker,
-          workerName: this._worker?.name,
-          workerDestroyed: this._worker?.destroyed,
-          hasTransport: !!this._transport,
-          stack: new Error().stack?.split('\n').slice(1, 6).join('; '),
-        });
-        return origTaskDestroy.apply(this, arguments as any);
-      };
-    } catch {}
-
     return pdfjsMod;
   })();
 
