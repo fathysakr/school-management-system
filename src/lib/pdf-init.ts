311c6b9a780c3cfba1652da8df1e3388b024b1dc
fathysakr <fathysakr@hotmail.com>
2026-07-01 17:04:47 +0300

    fix: patch PDFWorker.prototype.destroy to not null _messageHandler

--- src/lib/pdf-init.ts
+++ src/lib/pdf-init.ts
@@ -9,6 +9,16 @@ export async function getPdfjs(): Promise<any> {
     const workerMod = await import('./pdf.worker.mjs');
     (globalThis as any).pdfjsWorker = workerMod;
     const pdfjsMod = await import('pdfjs-dist/build/pdf.mjs');
+
+    pdfjsMod.PDFWorker.prototype.destroy = function () {
+      this.destroyed = true;
+      if (this._webWorker) {
+        this._webWorker.terminate();
+        this._webWorker = null;
+      }
+      this._port = null;
+    };
+
     return pdfjsMod;
   })();
 
